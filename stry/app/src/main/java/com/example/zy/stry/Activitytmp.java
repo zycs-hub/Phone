37690472a3bd1a58b584d4c9a6991787f7a5c4c6
zycs-hub <1098338793@qq.com>
2015-08-05 11:12:11 +0800

    改变布局

---
+++ stry/app/src/main/java/com/example/zy/stry/Activitytmp.java
@@ -0,0 +1,7 @@
+package com.example.zy.stry;
+
+/**
+ * Created by zy on 15/8/4.
+ */
+public class Activitytmp {
+}
---
+++ stry/app/src/main/java/com/example/zy/stry/util/LogStatusGlobla.java
@@ -0,0 +1,7 @@
+package com.example.zy.stry.util;
+
+/**
+ * Created by zy on 15/8/4.
+ */
+public class LogStatusGlobla {
+}
